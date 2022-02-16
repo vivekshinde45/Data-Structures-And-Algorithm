c08a85b75f0f95c4eaf7b712ea6dd1846ab971d8
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-02-16 09:26:54 +0530

    Create 20_Topological_Sort_Kahn's.cpp

---
+++ Graph/20_Topological_Sort_Kahn's.cpp
@@ -0,0 +1,44 @@
+https://practice.geeksforgeeks.org/problems/topological-sort/1
+
+
+
+class Solution{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> graph[]){
+	    // code here
+	    vector<int> indegree(V, 0);
+	    vector<int> ans;
+	    
+	    for(int i = 0; i < V; i++){
+	        for(auto const & e : graph[i]){
+	            indegree[e]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i = 0; i < V; i++){
+	        if(indegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    while(q.size()){
+	        auto rem = q.front();
+	        q.pop();
+	        
+	        ans.push_back(rem);
+	        
+	        for(auto const & e : graph[rem]){
+	            indegree[e]--;
+	            if(indegree[e] == 0){
+	                q.push(e);
+	            }
+	        }
+	    }
+	    if(ans.size() == V){
+	        return ans;
+	    }else{
+	        return {-1};
+	    }
+	}
+};
