44041f290576b443e0fe473199d82cfdcbc26af0
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-02-16 09:02:41 +0530

    Create 07_Knights_Tour.java

---
+++ Graph/07_Knights_Tour.java
@@ -0,0 +1,58 @@
+https://classroom.pepcoding.com/resources/online-java-foundation/graphs/knights-tour-official/ojquestion
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] chess = new int[n][n];
+        
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        
+        // int[] dirArr = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
+        printKnightsTour(chess, r, c, 1);
+    }
+
+    public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove) {
+        if(r < 0 || r >= chess.length || c < 0 || c >= chess.length || chess[r][c] != 0){
+            return;
+        }
+        
+        
+        if(upcomingMove == chess.length * chess.length){
+            chess[r][c] = upcomingMove;
+            displayBoard(chess);
+            chess[r][c] = 0;
+            return;
+        }
+        
+        chess[r][c] = upcomingMove;
+        printKnightsTour(chess, r - 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c + 2, upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c + 2, upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c - 1, upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c - 2, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c - 2, upcomingMove + 1);
+        printKnightsTour(chess, r - 2, c - 1, upcomingMove + 1);
+        chess[r][c] = 0;
+    }
+
+    public static void displayBoard(int[][] chess){
+        for(int i = 0; i < chess.length; i++){
+            for(int j = 0; j < chess[0].length; j++){
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
