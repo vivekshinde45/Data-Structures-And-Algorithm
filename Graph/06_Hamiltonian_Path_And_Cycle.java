2cc7dd5f6f311e1bfb9ac8ba05b3e9651bd3561c
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-02-16 09:01:20 +0530

    Create 06_Hamiltonian_Path_And_Cycle.java

---
+++ Graph/06_Hamiltonian_Path_And_Cycle.java
@@ -0,0 +1,68 @@
+https://classroom.pepcoding.com/resources/online-java-foundation/graphs/hamiltonian-official/ojquestion
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      int src = Integer.parseInt(br.readLine());
+      HashSet<Integer> hs = new HashSet<>();
+
+      printHamiltonian(graph, src, src + "", hs, src);
+   }
+   
+   public static void printHamiltonian(ArrayList<Edge>[] graph, int src, String psf, HashSet<Integer> hs, int osrc){
+       if(psf.length() == graph.length){
+           for(Edge e : graph[src]){
+               if(e.nbr == osrc){
+                   System.out.println(psf + "*");
+                   return;
+               }
+           }
+           System.out.println(psf + ".");
+           return;
+       }
+       hs.add(src);
+       for(Edge e : graph[src]){
+           if(hs.contains(e.nbr) == false){
+               printHamiltonian(graph, e.nbr, psf + e.nbr, hs, osrc);
+           }
+       }
+       hs.remove(src);
+   }
+
+
+}
