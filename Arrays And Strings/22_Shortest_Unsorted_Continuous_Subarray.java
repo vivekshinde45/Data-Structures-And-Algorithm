1c1d8b9a2b6d17fc0cbb16e69ebe8e685860db21
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-02-24 17:27:40 +0530

    Create 22_Shortest_Unsorted_Continuous_Subarray.java

---
+++ Arrays And Strings/22_Shortest_Unsorted_Continuous_Subarray.java
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+
+
+
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int end = -1;
+        int max = nums[0];
+        for(int i = 1; i < n; i++){
+            if(max > nums[i]){
+                end = i;
+            }
+            else{
+                max = nums[i];
+            }
+        }
+        int st = 0;
+        int min = nums[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] > min){
+                st = i;
+            }
+            else{
+                min = nums[i];
+            }
+        }
+        
+        return end - st + 1;
+    }
+}
